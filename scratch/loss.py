ba712109763c839d5f015aa310b289979d31ae67
ami-GS <1991.daiki@gmail.com>
2017-05-09 23:26:41 +0900

    fix trivial

--- scratch/loss.py
+++ scratch/loss.py
@@ -2,7 +2,7 @@ from layer import Layer
 import numpy as np
 
 class Loss(object):
-    def __init__(self, input_shape=None):
+    def __init__(self):
         pass
 
     def calc(self):
@@ -12,15 +12,15 @@ class Loss(object):
         pass
 
 class MSE(Loss):
-    def __init__(self, input_shape=None):
-        super(MSE, self).__init__(input_shape)
+    def __init__(self):
+        super(MSE, self).__init__()
 
     def calc(self, X, label):
         if len(X.shape) != 1 or X.shape != label.shape:
             print "loss error"
 
         Y = np.zeros(X.shape)
-        for i in range(self.input_shape):
+        for i in range(X.shape[0]):
             Y[i] = np.power(np.abs(X[i] - label[i]), 2)
 
         return np.sum(Y)*0.5
