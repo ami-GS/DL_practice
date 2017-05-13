a98afa592c33d59b4c3cedb4ece83db61b6eb4d9
ami-GS <1991.daiki@gmail.com>
2017-05-13 12:26:41 +0900

    implement ReLU

--- scratch/activation.py
+++ scratch/activation.py
@@ -23,3 +23,25 @@ class Sigmoid(Activation):
         for i in range(self.units):
             err_delta[i] *= self.Y[i] * (1 - self.Y[i])
         return err_delta
+
+class ReLU(Activation):
+    def __init__(self):
+        super(ReLU, self).__init__()
+
+    def forward(self, x):
+        for j in range(self.units):
+            self.Y[j] = max(0, x[j])
+
+        return self.Y
+
+    def backward(self, err_delta, learning_rate):
+        self.E = err_delta
+        for i in range(self.units):
+            if self.Y[i] > 0:
+                err_delta[i] *= 1
+            elif self.Y[i] < 0:
+                err_delta[i] = 0
+            else:
+                err_delta[i] *= np.random.uniform(0,1,1)
+
+        return err_delta
