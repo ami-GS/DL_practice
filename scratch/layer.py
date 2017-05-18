954d1111e22e98c2f5f776bde45ad3d3f422742c
ami-GS <1991.daiki@gmail.com>
2017-05-18 22:09:36 +0900

    partially implement Conv2D (no test)

--- scratch/layer.py
+++ scratch/layer.py
@@ -18,6 +18,40 @@ class Layer(object):
         pass
 
 
+class Conv2D(Layer):
+    def __init__(self, filters, channel, kernel_size, strides=(1,1), input_shape = None):
+        super(FullyConnect, self).__init__(input_shape, None)
+        self.filterNum = filters
+        self.kernel_size = kernel_size
+        self.filters = np.random.uniform(-1, 1, (channel, filters, kernel_size, kernel_size))
+        self.channel = 1 # WIP
+        self.strides = strides
+        # size is valid
+        self.units = int(np.sqrt(self.input_shape)) - self.kernel_size + 1
+        self.units *= self.units
+
+    def forward(self, x):
+        self.X = x
+         if x.shape >= 2:
+            self.channel = x.shape[0]
+
+        x_rowcol = int(np.sqrt(self.input_shape))
+        y_rowcol = x_rowcol - self.kernel_size+1
+
+        self.X = np.reshape(X, (channel, x_rowcol, x_rowcol))
+        self.Y = np.zeros((self.filterNum, y_rowcol, y_rowcol))
+        for f in range(self.filterNum):
+            for c in range(channel):
+                for xi in range(x_rowcol-self.kernel_size+1):
+                    for xj in range(x_rowcol-self.kernel_size+1):
+                        self.Y[f][xi][xj] = np.sum(np.multiply(X[c,xi:xi+self.kernel_size,xj:xj+self.kernel_size], self.filters[f,:,:]))
+
+        return self.Y
+
+    def backword(self, err_delta, learning_rate):
+        #TBD
+        pass
+
 class FullyConnect(Layer):
     def __init__(self, units, input_shape):
         super(FullyConnect, self).__init__(input_shape, units)
