4c41a8b0705b81b7d35763c37e8cacc2f30a5367
ami-GS <1991.daiki@gmail.com>
2017-05-23 22:26:41 +0900

    use Softmax and MaxPooling2D

--- scratch/app.py
+++ scratch/app.py
@@ -1,5 +1,5 @@
-from layer import FullyConnect
-from activation import Sigmoid
+from layer import FullyConnect, Conv2D, MaxPooling2D
+from activation import Sigmoid, ReLU, Tanh, Softmax
 from network import Network
 from loss import MSE
 import numpy as np
@@ -7,16 +7,21 @@ import numpy as np
 if __name__ == "__main__":
     input_shape = 2
     last_units = 1
-    epoch = 200000
+    epoch = 5000
 
     # XOR
     dataset = np.asarray([[0,0], [0,1], [1,0], [1,1]])
     label = np.asarray([[0],[1],[1],[0]])
     
+    # 8*8 image
+    input_shape = 64
+    dataset = np.random.rand(4, input_shape)
+
     net = Network(
-        [FullyConnect(units=32, input_shape=input_shape),
-         Sigmoid(),
-         FullyConnect(units=last_units, input_shape=32)])
+        [Conv2D(4, 1, 3, input_shape=input_shape),
+         Softmax(),
+         MaxPooling2D(2),
+         FullyConnect(units=last_units, input_shape=100)]) # 5*5* 4filter
 
     for e in range(epoch):
         if e ==0 or e == (epoch-1):
